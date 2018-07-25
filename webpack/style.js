6ee334c0b4e25d9a83379bc946383be62bdba34d
Alexy Stupakov <workinweb24@gmail.com>
2018-07-26 02:15:32 +0300

    Sass structure

--- webpack/style.js
+++ webpack/style.js
@@ -1,19 +1,46 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const autoprefixer = require('autoprefixer');
+const cssMqpacker = require('css-mqpacker');
 
 module.exports = function(env, argv){
 
+    let postCssplugins = [];
+
+    if (argv.mode == 'production'){
+        postCssplugins.push(
+            autoprefixer({
+                browsers:['ie >= 8', 'last 4 version']
+            })            
+        );
+
+        postCssplugins.push(
+            cssMqpacker()
+        );
+    }
+    
     return {
         module: {
             rules: [
                 {
                     test: /\.scss$/,
                     use: [
+
                         {
                             loader: MiniCssExtractPlugin.loader,
                         },
+
                         {
                             loader: 'css-loader',
                         },
+
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: postCssplugins,
+                                sourceMap: true
+                            }
+                        },
+
                         {
                             loader: 'sass-loader',
                         }
